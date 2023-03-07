83efc7c7ad63aeaf16a8851bb43616f21ff52673
joricky91 <jonathanricky91@gmail.com>
2023-03-07 14:04:57 +0700

    feat: creating NetworkManager class

---
+++ FetchDataCombine/Services/NetworkManager.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkManager.swift
+//  FetchDataCombine
+//
+//  Created by Jonathan Ricky Sandjaja on 07/03/23.
+//
+
+import Foundation
+import Combine
+
+class NetworkManager {
+    
+    static let shared = NetworkManager()
+    
+    
+    
+}
