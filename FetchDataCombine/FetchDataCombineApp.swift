3337208781fb0c697a60a12543f99c4d55f86f2b
joricky91 <jonathanricky91@gmail.com>
2023-03-07 14:01:18 +0700

    Initial Commit

---
+++ FetchDataCombine/FetchDataCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchDataCombineApp.swift
+//  FetchDataCombine
+//
+//  Created by Jonathan Ricky Sandjaja on 07/03/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchDataCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
